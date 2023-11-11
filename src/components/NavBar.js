d9d2a37d53272562a2b9b1afc30f48bfa51dae8f
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2023-11-11 22:07:45 +0000

    create state = menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,22 @@
-import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import React, {useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCity } from "@fortawesome/free-solid-svg-icons";
 
 export default function NavBar() {
 
-    return (
-        <>
-            <div className="mx-8 mt-8 container flex flex-row">
-                <div className="basis-1/4">Logo</div>
-                <div className="basis-1/2">Menu</div>
+    const {menu, setMenu} = useState('')
 
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="mx-8 mt-8 container flex flex-row">
+        <div className="basis-1/4">
+          <FontAwesomeIcon
+            className="text-green-500 text-3xl font-bold"
+            icon={faCity}
+          />
+        </div>
+        <div className="basis-1/2">Menu</div>
+      </div>
+    </>
+  );
 }
