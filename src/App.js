89f0ee1cc3003bf55aaeaa5242951fd060da59ca
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2023-11-11 23:07:54 +0000

    implement Routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import React, {useState} from "react";
 import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
 import Admin from './pages/Admin';
 import {Route, Routes} from 'react-router-dom';
+import PriceCalc from './pages/PriceCalc';
+import Contact from './pages/Contact';
 
 
 export default function App() {
 
-  const [menu, setMenu] = useState(
-    [
-      'Home',
-      'Price Calculator',
-      'Contact'
-    ]
-  )
+  const [menu, setMenu] = useState([
+    { name: 'HomePage', path: '/', component: HomePage },
+    { name: 'Price Calculator', path: '/price-calc', component: PriceCalc },
+    { name: 'Contact', path: '/contact', component: Contact },
+  ]);
 
   const handleMenu = (newMenu) => {
     setMenu(newMenu)
@@ -25,8 +26,10 @@ export default function App() {
       <div className="px-20">
       <NavBar menu={menu} />
       <Routes>
-        <Route></Route>
-      </Routes>
+      {menu.map(menuItem => (
+            <Route key={menuItem.path} path={menuItem.path} element={React.createElement(menuItem.component)} />
+          ))}
+        </Routes>
       </div>
     </>
   )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,9 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCity } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 
-export default function NavBar({menu}) {
-
+export default function NavBar({ menu }) {
   return (
     <>
       <div className="mx-8 mt-8 container flex flex-row">
@@ -14,13 +13,15 @@ export default function NavBar({menu}) {
             icon={faCity}
           />
         </div>
-        <div className="basis-1/2 flex flex-row content-center justify-around">
-            {menu && menu.map((menuItem) => (
-                <NavLink to={`/${menuItem.replace(/\s+/g, '').toLowerCase()}`} key={menuItem}>
-                {menuItem}
-              </NavLink>
-            ) )}
-        </div>
+        <nav className="basis-1/2 flex flex-row content-center justify-start">
+          <ul className="basis-1/2 flex flex-row content-center justify-around">
+            {menu.map((item) => (
+              <li key={item.path}>
+                <NavLink to={item.path}>{item.name}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </>
   );
