44d83a6f59ea2cc2a90b54bfbeccba748800b7bd
Bogdan <bogdan.burcea22@my.northampton.ac.uk>
2023-11-11 21:52:57 +0000

    New Page NavBar

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar'
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function NavBar() {
+
+    return (
+        <>
+            <div className="container flex flex-row">
+                <div className="basis-1/4">Logo</div>
+                <div className="basis-1/2">Menu</div>
+
+            </div>
+        </>
+    )
+}
